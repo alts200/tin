67143d67903675a27a7c20a456a83c907d32dd72
alts200 <125660276+alts200@users.noreply.github.com>
2024-01-06 13:35:38 +0300

    Update es6.js

--- task3/src/es6.js
+++ task3/src/es6.js
@@ -5,25 +5,57 @@
 
 // Напишите функцию, которая принимает ФИО пользователя и возвращает
 // строку формата Имя Фамилия
-function fioToName(fio) {}
+function fioToName(fio) {
+    var t = fio.split (' ');
+    var shortFIO = t [1] + ' ' + t [0];
+   return shortFIO
+}
 
 // преобразуйте массив чисел так, чтобы в нем остались только
 // уникальные элементы
 // присмотритесь к коллекции "Set"
-function filterUnique(array) {}
+function filterUnique(array) {
+    const unique = [...new Set(array)];
+     return unique;
+}
 
 // Задача: разница зарплат
 // в функцию приходит массив из n зарплат сотрудников фирмы
 // ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
 // сотрудника превышает зарплату самого низкооплачиваемого
-function calculateSalaryDifference(array) {}
+function calculateSalaryDifference(array) {
+    let minValue = Math.min(...array);
+    let maxValue = Math.max(...array);
+    var result = maxValue/minValue;
+    return result;
+}
 
 // Реализуйте класс "словарь слов" (как толковый словарь)
 // класс должен быть безопасным и работать только со словами
 // присмотритесь к коллекции "Map"
 // Словарь - (string, string), и все это не null и не undefined
 // * покройте класс тестами
-class Dictionary {}
+class Dictionary {
+    #dict;
+    constructor() {
+        this.#dict = new Map();
+    }
+
+    set(key, value){
+        if( typeof(key) === 'string' && typeof(value) === 'string'){
+            this.#dict.set(key, value);
+            return true;
+        }
+        return false;
+    }
+
+    get(key){
+        if( typeof(key) === 'string'){
+            return this.#dict.get(key)
+        }
+        return false;
+    }
+}
 
 module.exports = {
     fioToName,
@@ -31,3 +63,4 @@ module.exports = {
     Dictionary,
     calculateSalaryDifference
 };
+
